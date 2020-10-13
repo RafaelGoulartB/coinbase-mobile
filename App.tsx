1281c9a84a28ba7d8dda0621208f970448cb78d5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-13 08:10:28 -0300

    📦 feat: add typescript project with expo

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
