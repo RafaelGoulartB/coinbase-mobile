a9f4d4b9e3794bfcb5138538f2cb07d09ed36a02
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-26 09:01:46 -0300

    feat: add base compoments

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Routes from './src/routes'
+import { StatusBar } from 'expo-status-bar'
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar />
       <Routes />
     </NavigationContainer>
   )
---
+++ src/style.ts
@@ -0,0 +1,14 @@
+import Constants from 'expo-constants'
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  height: 100%;
+  padding-top: ${Constants.statusBarHeight + 20}px;
+  align-items: center;
+`
+export const ScrollContainer = styled.ScrollView`
+  flex: 1;
+  height: 100%;
+  padding-top: ${Constants.statusBarHeight + 20}px;
+`
