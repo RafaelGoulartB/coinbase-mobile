205fbb279419a1f3b86f9592ec8c241c6d1ee18c
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-10 09:25:00 -0300

    fix: remove header from auth pages

--- src/routes/auth.route.tsx
+++ src/routes/auth.route.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
-import Onboarding from '../pages/onboarding/'
+import Onboarding from '../pages/auth/onboarding'
 
 const Stack = createStackNavigator()
 
 const AuthRoute: React.FC = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Onboard" headerMode="none">
       <Stack.Screen name="Onboard" component={Onboarding} />
       <Stack.Screen name="Login" component={() => <View />} />
       <Stack.Screen name="Register" component={() => <View />} />
